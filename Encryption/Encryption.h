2b14b72ff6baca4522be68c1505a2f0ed34e009e
HLRichardson <101138530+HLRichardson-Git@users.noreply.github.com>
2023-01-09 19:05:26 -0600

    Update Encryption.h

--- Encryption/Encryption.h
+++ Encryption/Encryption.h
@@ -1,1 +1,27 @@
+#pragma once
 
+#include "KeySchedule.h"
+#include "helper.h"
+
+class Encryption
+{
+public:
+
+	unsigned char cipherText[4][4];
+
+	void run();
+
+private:
+
+	KeySchedule key;
+	Help help;
+	bool debug = 1; // BOOL DEBUG ACTIVE
+
+	void subByte();
+	void shiftRows();
+	void mixColumns();
+	void addRoundKey(int round);
+
+	void initializeCipherText();
+
+};

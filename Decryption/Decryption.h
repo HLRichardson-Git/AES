64a11991849e124b2ff6a09872a0f766bc0342bf
HLRichardson <101138530+HLRichardson-Git@users.noreply.github.com>
2023-01-14 19:39:57 -0600

    Update Decryption.h

--- Decryption/Decryption.h
+++ Decryption/Decryption.h
@@ -1,1 +1,31 @@
+#pragma once
 
+#include "KeySchedule.h"
+#include "helper.h"
+
+class Decryption
+{
+public:
+
+	unsigned char plainText[4][4];
+
+	void run();
+
+private:
+
+	KeySchedule key;
+	Help help;
+	bool debug = 1; // BOOL DEBUG ACTIVE
+	unsigned char nist[4][4] = { {0x6d, 0x25, 0x1e, 0x69},
+								 {0x44, 0xb0, 0x51, 0xe0},
+								 {0x4e, 0xaa, 0x6f, 0xb4},
+								 {0xdb, 0xf7, 0x84, 0x65} };
+
+	void invSubByte();
+	void invShiftRows();
+	void invMixColumns();
+	void invAddRoundKey(int round);
+
+	void initializePlainText();
+
+};
